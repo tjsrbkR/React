52d8ef32aa369e727fab56df308e0c5f4bfaa7af
tjsrbKR <kkil5205@gmail.com>
2023-07-18 22:57:27 +0900

    로그인 디자인 완료

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { Link } from 'react-router-dom';
 import "./Login.css"
 function Login(props) {
     const mainBg = {
@@ -19,7 +20,7 @@ function Login(props) {
                         <fieldset className='info'>
                             <section>
                                 <p className='welcome'>Welcome! 스타벅스 코리아에 오신걸 환영합니다.</p>
-                                <div>
+                                <div className='info-'>
                                     <div className='login'>
                                         <input type="text" name="id" placeholder='아이디를 입력해 주세요' />
                                         <input type="password" name="pw" placeholder='비밀번호를 입력해 주세요' />
@@ -32,6 +33,24 @@ function Login(props) {
                                         </label>
                                     </div>
                                     <input type="submit" value="로그인" />
+                                </div>
+                                <p className='waring-txt'>
+                                    * 타 사이트와 비밀번호를 동일하게 사용할 경우 도용의 위험이 있으므로, 정기적인 비밀번호 변경을 해주시길 바랍니다.
+                                    <br />
+                                    * 스타벅스 코리아의 공식 홈페이지는 Internet Explorer 9.0 이상, Chrome, Firefox, Safari 브라우저에 최적화 되어있습니다.
+                                </p>
+                                <div className='forget'>
+                                    <ul>
+                                        <li>
+                                            회원가입
+                                        </li>
+                                        <li>
+                                            아이디 찾기
+                                        </li>
+                                        <li>
+                                            비밀번호 찾기
+                                        </li>
+                                    </ul>
                                 </div>
                             </section>
                         </fieldset>
